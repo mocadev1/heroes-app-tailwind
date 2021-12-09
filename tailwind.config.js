980bdd8fc6ba5b9e5b21c96ff0453f0b35740698
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-12-09 14:26:45 -0600

    Added Navbar using React Router and Tailwind for styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-    mode: 'jit',
     purge: [ './src/**/*.{js,jsx,ts,tsx}', './public/index.html' ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        container: {
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem'
+            },
+        },
+        extend: {
+        },
     },
     variants: {
-        extend: {},
+        extend: {
+            textColor: ['active']
+        },
     },
     plugins: [],
 }
