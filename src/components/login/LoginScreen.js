38bb21f8d1261ee3eb1c94635b04b30ea07f97a6
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2022-01-04 15:04:41 -0600

    Login & logout.
    With useContext and useReducer for the global application state.

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,14 +1,29 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { types } from '../../types/types';
+import { AuthContext } from '../../auth/authContext';
 
 export const LoginScreen = () => {
 
     const navigate = useNavigate();
 
+    const {dispatch} = useContext(AuthContext);
+
     const handleLogin = () => {
+        const action = {
+            type: types.login,
+            payload: {
+                name: 'Omar'
+            }
+        }
+
+        dispatch(action);
 
         navigate('/', {
             replace: true
         });
+
 
     };
 
